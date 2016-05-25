83cae6b9e2b26492c30e7312ba08305a078e78e5
carlos-araque <c.araque29@gmail.com>
2016-05-25 19:51:10 +0200

    Añadir imagenes

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,8 +1,14 @@
 var models = require('../models');
 var Sequelize = require('sequelize');
+var cloudinary = require('cloudinary');
+var fs = require('fs');
+
+// Opciones para imagenes subidas a Cloudinary
+var cloudinary_image_options = { crop: 'limit', width: 200, height: 200, radius: 5,
+                                 border: "3px_solid_blue", tags: ['core', 'quiz-2016'] };
 
 exports.load = function(req, res, next, quizId) {
-  models.Quiz.findById(quizId, {include: [models.Comment]})
+  models.Quiz.findById(quizId, {include: [models.Comment, models.Attachment]})
   .then(function(quiz) {
     if(quiz){
       req.quiz = quiz;
@@ -37,7 +43,7 @@ exports.index = function(req, res, next) {
             });
     }).catch(function(error){next(error);});
   }else{
-  models.Quiz.findAll().then(function(quizzes) {
+  models.Quiz.findAll({ include: [ models.Attachment ]}).then(function(quizzes) {
     if(req.params.format==="json"){
       res.json({ quizzes});
     }else{
@@ -86,28 +92,39 @@ exports.new = function(req, res, next) {
 
 //POST create
 exports.create = function(req, res, next) {
+   var redir = req.body.redir || '/quizzes';
   var authorId = req.session.user && req.session.user.id || 0;
-  var quiz = models.Quiz.build({
+  var quiz = {
     question: req.body.question,
     answer: req.body.answer,
     tematica: req.body.tematica,
     AuthorId: authorId
-  });
-  quiz.save({fields: ["question", "answer", "tematica", "AuthorId"]})
-    .then(function(quiz) {
-      req.flash('success', 'Pregunta creada con éxito');
-      res.redirect('/quizzes');
-    }).catch(Sequelize.ValidationError, function(error) {
+  };
+  // Guarda en la tabla Quizzes el nuevo quiz.
+  models.Quiz.create(quiz).then(function(quiz) {
+      req.flash('success', 'Pregunta y Respuesta guardadas con éxito.');
+      if (!req.file) {
+          req.flash('info', 'Es un Quiz sin imagen.');
+          return;
+      }
+      // Salvar la imagen en Cloudinary
+      return uploadResourceToCloudinary(req).then(function(uploadResult) {
+          // Crear nuevo attachment en la BBDD.
+          return createAttachment(req, uploadResult, quiz);
+      });
+  }).then(function() {
+      res.redirect(redir);
+  }).catch(Sequelize.ValidationError, function(error) {
       req.flash('error', 'Errores en el formulario:');
       for (var i in error.errors) {
-        req.flash('error', error.errors[i].value);
+          req.flash('error', error.errors[i].value);
       };
-      res.render('quizzes/new');
-    })
-    .catch(function(error) {
-      req.flash('error', 'Error al crear pregunta: '+error.message);
+      res.render('quizzes/new', {quiz: quiz,
+      redir:redir});
+  }).catch(function(error) {
+      req.flash('error', 'Error al crear un Quiz: '+error.message);
       next(error);
-    });
+  });
 };
 
 //GET edit
@@ -118,18 +135,36 @@ exports.edit = function(req, res, next){
 };
 //PUT /quizzes/:id
 exports.update = function(req, res, next) {
+   var redir = req.body.redir || '/quizzes';
   req.quiz.question = req.body.question;
   req.quiz.answer = req.body.answer;
-  req.quiz.save({fields: ["question", "answer"]}).then(function(quiz){
+  req.quiz.tematica = req.body.tematica;
+  req.quiz.save({fields: ["question", "answer", "tematica"]}).then(function(quiz){
     req.flash('success', 'Pregunta modificada con éxito');
-    res.redirect('/quizzes');
+        // Sin imagen: Eliminar attachment e imagen viejos.
+        if (!req.file) {
+            req.flash('info', 'Tenemos un Quiz sin imagen.');
+            if (quiz.Attachment) {
+                cloudinary.api.delete_resources(quiz.Attachment.public_id);
+                return quiz.Attachment.destroy();
+            }
+            return;
+        }
+        // Salvar la imagen nueva en Cloudinary
+        return uploadResourceToCloudinary(req).then(function(uploadResult) {
+            // Actualizar el attachment en la BBDD.
+            return updateAttachment(req, uploadResult, quiz);
+        });
+    }).then(function() {
+        res.redirect(redir);
   }).catch(Sequelize.ValidationError, function(error) {
     req.flash('error', 'Errores en el formulario:');
     for(var i in error.errors){
       req.flash('error', error.errors[i].value);
     };
     res.render('quizzes/edit', {
-      quiz: req.quiz
+      quiz: req.quiz,
+      redir: redir
     });
   }).catch(function(error) {
     req.flash('error', 'Error al editar el Quiz:'+ error.message);
@@ -160,3 +195,87 @@ exports.ownershipRequired = function(req,res,next){
     res.send(403);
   }
 };
+
+//FUNCIONES AUXILIARES
+
+/**
+ * Crea una promesa para crear un attachment en la tabla Attachments.
+ */
+function createAttachment(req, uploadResult, quiz) {
+    if (!uploadResult) {
+        return Promise.resolve();
+    }
+
+    return models.Attachment.create({ public_id: uploadResult.public_id,
+                                      url: uploadResult.url,
+                                      filename: req.file.originalname,
+                                      mime: req.file.mimetype,
+                                      QuizId: quiz.id
+    }).then(function(attachment) {
+        req.flash('success', 'Imagen nueva guardada con éxito.');
+    }).catch(function(error) { // Ignoro errores de validacion en imagenes
+        req.flash('error', 'No se ha podido salvar la nueva imagen: '+error.message);
+        cloudinary.api.delete_resources(uploadResult.public_id);
+    });
+}
+
+
+/**
+ * Crea una promesa para actualizar un attachment en la tabla Attachments.
+ */
+function updateAttachment(req, uploadResult, quiz) {
+    if (!uploadResult) {
+        return Promise.resolve();
+    }
+
+    // Recordar public_id de la imagen antigua.
+    var old_public_id = quiz.Attachment ? quiz.Attachment.public_id : null;
+
+    return quiz.getAttachment().then(function(attachment) {
+        if (!attachment) {
+            attachment = models.Attachment.build({ QuizId: quiz.id });
+        }
+        attachment.public_id = uploadResult.public_id;
+        attachment.url = uploadResult.url;
+        attachment.filename = req.file.originalname;
+        attachment.mime = req.file.mimetype;
+        return attachment.save();
+    }).then(function(attachment) {
+        req.flash('success', 'Imagen nueva guardada con éxito.');
+        if (old_public_id) {
+            cloudinary.api.delete_resources(old_public_id);
+        }
+    })
+    .catch(function(error) { // Ignoro errores de validacion en imagenes
+        req.flash('error', 'No se ha podido salvar la nueva imagen: '+error.message);
+        cloudinary.api.delete_resources(uploadResult.public_id);
+    });
+}
+
+
+/**
+ * Crea una promesa para subir una imagen nueva a Cloudinary.
+ * Tambien borra la imagen original.
+ *
+ * Si puede subir la imagen la promesa se satisface y devuelve el public_id y
+ * la url del recurso subido.
+ * Si no puede subir la imagen, la promesa tambien se cumple pero devuelve null.
+ *
+ * @return Devuelve una Promesa.
+ */
+function uploadResourceToCloudinary(req) {
+    return new Promise(function(resolve,reject) {
+        var path = req.file.path;
+        cloudinary.uploader.upload(path, function(result) {
+                fs.unlink(path); // borrar la imagen subida a ./uploads
+                if (! result.error) {
+                    resolve({ public_id: result.public_id, url: result.secure_url });
+                } else {
+                    req.flash('error', 'No se ha podido salvar la nueva imagen: '+result.error.message);
+                    resolve(null);
+                }
+            },
+            cloudinary_image_options
+        );
+    })
+}
--- models/index.js
+++ models/index.js
@@ -24,16 +24,32 @@ var sequelize = new Sequelize(url,
 				          omitNull: true
 });
 
+// Importar la definicion de la tabla Quiz de quiz.js
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+
+// Importar la definicion de la tabla Comments de comment.js
 var Comment = sequelize.import(path.join(__dirname,'comment'));
+
+// Importar la definicion de la tabla Users de user.js
 var User = sequelize.import(path.join(__dirname,'user'));
 
+// Importar la definicion de la tabla Attachments de attachment.js
+var Attachment = sequelize.import(path.join(__dirname,'attachment'));
+
+// Relaciones entre modelos
 Comment.belongsTo(Quiz);
 Quiz.hasMany(Comment);
 
-Quiz.belongsTo(User, {as: 'Author', foreignKey: 'AuthorId'});
+// Relacion 1 a N entre User y Quiz:
 User.hasMany(Quiz, {foreignKey: 'AuthorId'});
+Quiz.belongsTo(User, {as: 'Author', foreignKey: 'AuthorId'});
 
-exports.Quiz = Quiz;
-exports.Comment = Comment;
-exports.User = User;
+// Relacion 1-a-1 ente Quiz y Attachment
+Attachment.belongsTo(Quiz);
+Quiz.hasOne(Attachment);
+
+
+exports.Quiz = Quiz;       // exportar definición de tabla Quiz
+exports.Comment = Comment; // exportar definición de tabla Comments
+exports.User = User;       // exportar definición de tabla Users
+exports.Attachment = Attachment; // exportar definición de tabla Attachments
